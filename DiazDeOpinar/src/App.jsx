d2d6b88401a54fe211524479a4b9d12d7ba43f4e
JezrrelValles <jezrrel.valles@ia.center>
2024-05-20 01:56:40 -0600

    Hotfix

--- DiazDeOpinar/src/App.jsx
+++ DiazDeOpinar/src/App.jsx
@@ -1,17 +1,25 @@
-import Footer from "./components/Footer"
-import Card from "./components/Card"
+import React from 'react';
+import Footer from './components/Footer';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="flex flex-col md:flex-row justify-center items-center md:mb-16">
       <Card
         image="./gradient.png"
-        buttonText="Haz clic aquí para descubrir más"
+        buttonText={
+          <>
+            Nos interesa tu opinión
+            <br />
+            <br />
+            Haz clic aquí para descubrir más
+          </>
+        }
         className="md:mr-8 mb-8 md:mb-0" // Agrega margen derecho solo en pantallas medianas y grandes
       />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- DiazDeOpinar/src/components/Draw.jsx
+++ DiazDeOpinar/src/components/Draw.jsx
@@ -2,19 +2,14 @@ import React from 'react';
 import CanvasDraw from 'react-canvas-draw';
 
 const DrawingInput = () => {
-  // Calculamos el ancho del lienzo basado en el ancho de la ventana
-  const canvasWidth = window.innerWidth * 0.8;
-  // Fijamos una altura relativa para mantener la proporción
-  const canvasHeight = canvasWidth * 0.5;
-
   return (
     <div className="p-4 m-4 bg-gray-100 rounded-lg shadow-md">
       <h2 className="text-lg font-semibold mb-4">Dibuja tu propuesta</h2>
-      <div style={{ maxWidth: '100%' }}>
+      <div className="overflow-hidden rounded-md border border-gray-300 max-w-full sm:max-w-md lg:max-w-lg xl:max-w-xl">
         <CanvasDraw
-          className="border border-gray-300 rounded-md"
-          canvasWidth={canvasWidth}
-          canvasHeight={canvasHeight}
+          className="w-full h-64 sm:h-48 lg:h-64 xl:h-80" // Ajusta las alturas para diferentes tamaños de pantalla
+          canvasWidth={600}
+          canvasHeight={300}
           loadTimeOffset={10}
         />
       </div>
